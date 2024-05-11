90e48cd9e9aa93cc39769f9094f2646586d048ce
Nishanth-S1142001 <54067788+Nishanth-S1142001@users.noreply.github.com>
2024-05-11 13:29:22 +0530

    Done with snack bar

--- app/src/main/java/com/example/todo/DummyFragment.java
+++ app/src/main/java/com/example/todo/DummyFragment.java
@@ -3,9 +3,10 @@ package com.example.todo;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
-
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.fragment.app.Fragment;
-
+import com.google.android.material.snackbar.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,17 +16,34 @@ import android.widget.ImageView;
 public class DummyFragment extends Fragment implements View.OnClickListener{
     Button pop;
     ViewGroup container1;
+    Button snackbutton;
+    ConstraintLayout layout;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
         super.onCreateView(inflater, container, savedInstanceState);
         container1 = container;
         View view = inflater.inflate(R.layout.fragment_dummy, container, false);
-        System.out.println(view);
+        snackbutton = view.findViewById(R.id.snackBarTest);
+        layout = view.findViewById(R.id.dummyFragment);
+        System.out.println(view + "view");
         pop = view.findViewById(R.id.pop);
-        System.out.println(pop);
-        pop.setOnClickListener(this);
+        System.out.println(pop + "pop");
+        pop.setOnClickListener(new View.OnClickListener() {
+                                   @Override
+                                   public void onClick(View view) {
+                                       showDialog(container1);
+                                   }
+                               });
+                snackbutton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        snackBar();
+                    }
+                });
         return view;
+
     }
     private void showDialog(ViewGroup container) {
 
@@ -34,19 +52,29 @@ public class DummyFragment extends Fragment implements View.OnClickListener{
         dialog.setContentView(R.layout.popup);
         dialog.getWindow().setBackgroundDrawableResource(R.drawable.bt);
         ImageView btnClose = dialog.findViewById(R.id.btn_close);
-        btnClose.setOnClickListener(new View.OnClickListener() {
+        btnClose.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)
+            {
                 dialog.dismiss();
             }
-        });
+        }
+        );
 
         dialog.show();
     }
+    private void snackBar()
+    {
+        Snackbar snackbar
+                = Snackbar
+                .make(  layout,
+                        "Snack Message is here!",
+                        Snackbar.LENGTH_LONG);
+        snackbar.show();
+    }
     @Override
     public void onClick(View v) {
-        //do what you want to do when button is clicked
-
-        showDialog(container1);
-    }
+        //do not remove this method, it is used to implement View.OnclickListener
+            }
 }
